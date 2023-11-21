9aad5d665874e25c429e29ca8b12f6249fba9d5b
hoyeon-paywork <hoyeon@paywork.io>
2023-11-21 15:41:43 +0900

    test

--- src/websocket.ts
+++ src/websocket.ts
@@ -12,20 +12,27 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 	const connectionId = context.connectionId;
 	
 	switch(context.eventType) {
-		case EventType.CONNECT:
+		case EventType.CONNECT: {
 			console.log(`Connection established: ${connectionId}`);
 			break;
-		case EventType.DISCONNECT:
+		}
+		
+		case EventType.DISCONNECT: {
 			console.log(`Connection closed: ${connectionId}`);
 			break;
-		case EventType.MESSAGE:
+		}
+			
+		case EventType.MESSAGE: {
 			console.log(`Message received from ${connectionId}: ${event.body}`);
 			break;
-		default:
+		}
+			
+		default: {
 			console.log(`Unknown event type: ${context.eventType}`);
 			break;
+		}
 	}
-	
+
 	return {
 		statusCode: 200,
 		body: "OK",
